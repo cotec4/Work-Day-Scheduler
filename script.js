d1b4e7fcad75f57720aba642cd0e135728d74d70
Connor Cote <cotec4@gmail.com>
2020-07-15 17:57:30 -0400

    HTML and CSS complete, date and time operational in JS as well as function for changing class of timeblocks working

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var currentDay = document.getElementById("currentDay");
+
+var row1 = document.getElementById("9");
+var row2 = document.getElementById("10");
+var row3 = document.getElementById("11");
+var row4 = document.getElementById("12");
+var row5 = document.getElementById("13");
+var row6 = document.getElementById("14");
+var row7 = document.getElementById("15");
+var row8 = document.getElementById("16");
+var row9 = document.getElementById("17");
+var rows = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
+
+// Get Current Date as well as hour to input time and date and to use hours on changing textarea class
 var date = new Date(),
     weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
     dayOfWeek = weekday[date.getDay()],
@@ -5,6 +19,23 @@ var date = new Date(),
     dayOfMonth = today + (date.getDate() < 10) ? '0' + date.getDate() + domEnder : date.getDate() + domEnder,
     months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
     curMonth = months[date.getMonth()],
+    curHour = date.getHours(),
     today = dayOfWeek + "," + " " + curMonth + " " + dayOfMonth;
-var currentDay = document.getElementById("currentDay");
 currentDay.innerHTML = today;
+
+console.log(row9.classList);
+
+function changeClass() {
+    for (var i = 0; i <rows.length; i++) {
+        if (curHour === parseInt(rows[i].id)) {
+            rows[i].classList.add("present");
+        }
+        else if (curHour < parseInt(rows[i].id)) {
+            rows[i].classList.add("future");
+        }
+        else if (curHour > parseInt(rows[i].id)) {
+            rows[i].classList.add("past");
+        }
+    }
+}
+changeClass();
