bf55efef1424e8e32e83ccef9012ad48d0d42d25
Connor Cote <cotec4@gmail.com>
2020-07-15 22:22:58 -0400

    Added in JS functionality to get local storage operational and ensure the events stay in each timeblock even upon refreshing

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var currentDay = document.getElementById("currentDay");
 
+var time1 = document.getElementById("time1");
+var time2 = document.getElementById("time2");
+var time3 = document.getElementById("time3");
+var time4 = document.getElementById("time4");
+var time5 = document.getElementById("time5");
+var time6 = document.getElementById("time6");
+var time7 = document.getElementById("time7");
+var time8 = document.getElementById("time8");
+var time9 = document.getElementById("time9");
+var times = [time1, time2, time3, time4, time5, time6, time7, time8, time9];
+
 var row1 = document.getElementById("9");
 var row2 = document.getElementById("10");
 var row3 = document.getElementById("11");
@@ -23,10 +34,9 @@ var date = new Date(),
     today = dayOfWeek + "," + " " + curMonth + " " + dayOfMonth;
 currentDay.innerHTML = today;
 
-console.log(row9.classList);
-
+// Function to change the class (and therefore formatting) for each Timeblock
 function changeClass() {
-    for (var i = 0; i <rows.length; i++) {
+    for (var i = 0; i < rows.length; i++) {
         if (curHour === parseInt(rows[i].id)) {
             rows[i].classList.add("present");
         }
@@ -39,3 +49,24 @@ function changeClass() {
     }
 }
 changeClass();
+
+// Save the Data to local storage
+function saveData(event) {
+
+    var placeToSave = $(this).parent().children()[1];
+    var textToSave = $(this).parent().children()[1].value;
+    var keyToSave = $(this).parent().children()[0].textContent;
+
+    if (textToSave !== "") {
+        localStorage.setItem(keyToSave, textToSave);
+        placeToSave.innerHTML = textToSave;
+    }
+}
+
+// Ensure the Saved Data stays on the page even when refreshed
+for (var i = 0; i < times.length; i++) {
+    $(rows[i]).text(localStorage.getItem(times[i].textContent));
+}
+
+// On Click of save move data to local storage
+$(document).on("click", ".saveBtn", saveData);
